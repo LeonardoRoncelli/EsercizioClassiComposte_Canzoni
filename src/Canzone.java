8bce93ecedfe27cd463f3dd074a0f8eb64b7f942
Roncelli <leonardoroncelli@gmail.com>
2024-10-22 15:08:29 +0200

    Metodi set e get di tutti gli attributi.

--- src/Canzone.java
+++ src/Canzone.java
@@ -1,3 +1,23 @@
 public class Canzone {
-
+    private String nomeCanzone;
+    private String nomeCantante;
+    private double durata;
+    public void setNomeCanzone(String nomeCanzone){
+        this.nomeCanzone=nomeCanzone;
+    }
+    public String getNomeCanzone(){
+        return nomeCanzone;
+    }
+    public void setNomeCantante(String nomeCantante){
+        this.nomeCantante=nomeCantante;
+    }
+    public String getNomeCantante(){
+        return nomeCantante;
+    }
+    public void setDurata(double durata){
+        this.durata=durata;
+    }
+    public double getDurata(){
+        return durata;
+    }
 }
