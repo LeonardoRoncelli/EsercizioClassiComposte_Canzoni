9098006b97236d5c3f8c0a76c6fc5d6f828ec1a6
Roncelli <leonardoroncelli@gmail.com>
2024-10-22 15:23:15 +0200

    Metodi set e get della classe Playlist.

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,3 +1,12 @@
 public class Playlist {
+    private Canzone[]miaPlaylist;
+
+    public Canzone[] getMiaPlaylist() {
+        return miaPlaylist;
+    }
+
+    public void setMiaPlaylist(Canzone[] miaPlaylist) {
+        this.miaPlaylist = miaPlaylist;
+    }
 
 }
