fa2749501233396711dc494f206595f40d691e92
Roncelli <leonardoroncelli@gmail.com>
2024-10-22 15:25:30 +0200

    Costruttore classe Playlist.

--- src/Playlist.java
+++ src/Playlist.java
@@ -8,5 +8,7 @@ public class Playlist {
     public void setMiaPlaylist(Canzone[] miaPlaylist) {
         this.miaPlaylist = miaPlaylist;
     }
-
+    public Playlist(){
+        Canzone[]canzoni=new Canzone[100];
+    }
 }
