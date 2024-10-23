a291a54de2f214d2be8f201149f433fcf21ebfb9
Roncelli <leonardoroncelli@gmail.com>
2024-10-23 22:00:33 +0200

    Correzioni e classe TestPlaylist completata.

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,112 +1,79 @@
 public class Playlist {
-    private Canzone[]miaPlaylist;
+    private Canzone[] miaPlaylist;
     private int lunghezza;
 
     public Canzone[] getMiaPlaylist() {
         return miaPlaylist;
     }
-
     public void setMiaPlaylist(Canzone[] miaPlaylist) {
         this.miaPlaylist = miaPlaylist;
     }
-    public Playlist(){
-        lunghezza=100;
-        miaPlaylist=new Canzone[lunghezza];
+    public Playlist() {
+        lunghezza = 100;
+        miaPlaylist = new Canzone[lunghezza];
     }
-    public int numeroCanzoni(){
-        int conta=0;
-        for(int i=0;i<miaPlaylist.length;i++){
-            if(miaPlaylist[i]!=null){
+    public int numeroCanzoni() {
+        int conta = 0;
+        for (int i = 0; i < miaPlaylist.length; i++) {
+            if (miaPlaylist[i] != null) {
                 conta++;
             }
         }
         return conta;
     }
-    public void aggiungiCanzone(Canzone nuovaCanzone){
-        int contaOccupato=0;
-        boolean pieno=false;
-        for(int i=0;i<miaPlaylist.length;i++){
-            if(miaPlaylist[i]!=null){
-                contaOccupato++;
-                if (contaOccupato==miaPlaylist.length){
-                    System.out.println("La playlist è piena");
-                    pieno=true;
-                    break;
-                }
+    public void aggiungiCanzone(Canzone nuovaCanzone) {
+        boolean pieno = true;
+        for (int i = 0; i < miaPlaylist.length; i++) {
+            if (miaPlaylist[i] == null) {
+                miaPlaylist[i] = nuovaCanzone;
+                pieno = false;
+                break;
             }
         }
-        if (!pieno){
-            for(int i=0;i<miaPlaylist.length;i++){
-                if(miaPlaylist[i]==null){
-                    miaPlaylist[i]=nuovaCanzone;
-                    break;
-                }
+        if (pieno) {
+            System.out.println("La playlist è piena");
+        }
+    }
+    public void aggiungiCanzone(String nomeCanzone, String nomeCantante, double durata) {
+        Canzone nuovaCanzone = new Canzone(nomeCanzone, nomeCantante, durata);
+        aggiungiCanzone(nuovaCanzone);
+    }
+    public void cancellaCanzone(Canzone canzoneCanc) {
+        for (int i = 0; i < miaPlaylist.length; i++) {
+            if (miaPlaylist[i] != null && miaPlaylist[i].equals(canzoneCanc)) {
+                miaPlaylist[i] = null;
+                return;
             }
         }
     }
-    public void aggiungiCanzone(String nomeCanzone,String nomeCantante, double durata){
-        int contaOccupato=0;
-        boolean pieno=false;
-        Canzone nuovaCanzone=new Canzone(nomeCanzone,nomeCantante,durata);
-        for(int i=0;i<miaPlaylist.length;i++){
-            if(miaPlaylist[i]!=null){
-                contaOccupato++;
-                if (contaOccupato==miaPlaylist.length){
-                    System.out.println("La playlist è piena");
-                    pieno=true;
-                    break;
-                }
+    public String ricercaCantante(String nomeCanzone) {
+        for (int i = 0; i < miaPlaylist.length; i++) {
+            if (miaPlaylist[i] != null && miaPlaylist[i].getNomeCanzone().equals(nomeCanzone)) {
+                return miaPlaylist[i].getNomeCantante();
             }
         }
-        if (!pieno){
-            for(int i=0;i<miaPlaylist.length;i++){
-                if(miaPlaylist[i]==null){
-                    miaPlaylist[i]=nuovaCanzone;
-                    break;
-                }
-            }
-        }
+        return "";
     }
-    public void cancellaCanzone(Canzone canzoneCanc){
-        Canzone temp=new Canzone();
-        for(int i=0;i<miaPlaylist.length;i++){
-            if(miaPlaylist[i].equals(canzoneCanc)){
-                temp=miaPlaylist[miaPlaylist.length-1];
-                miaPlaylist[miaPlaylist.length-1]=miaPlaylist[i];
-                miaPlaylist[i]=temp;
-                lunghezza--;
-            }
-        }
-    }
-    public String ricercaCantante(String nomeCanzone){
-        String cantante="";
-        for(int i=0;i<miaPlaylist.length;i++){
-            if(miaPlaylist[i].equals(nomeCanzone)){
-                cantante=miaPlaylist[i].getNomeCantante();
-            }
-        }
-        return cantante;
-    }
-    public Canzone[] ricercaCanzoni(String nomeCantante){
-        Canzone[]canzoniTrovate=new Canzone[miaPlaylist.length];
-        int conta=0;
-        for(int i=0;i<miaPlaylist.length;i++){
-            if(miaPlaylist[i]!=null&&miaPlaylist[i].getNomeCantante().equals(nomeCantante)){
-                canzoniTrovate[conta]=miaPlaylist[i];
+    public Canzone[] ricercaCanzoni(String nomeCantante) {
+        Canzone[] canzoniTrovate = new Canzone[miaPlaylist.length];
+        int conta = 0;
+        for (int i = 0; i < miaPlaylist.length; i++) {
+            if (miaPlaylist[i] != null && miaPlaylist[i].getNomeCantante().equals(nomeCantante)) {
+                canzoniTrovate[conta] = miaPlaylist[i];
                 conta++;
             }
         }
-        Canzone[]finale=new Canzone[conta];
-        for(int i=0;i<conta;i++){
-            finale[i]=canzoniTrovate[i];
+        Canzone[] finale = new Canzone[conta];
+        for (int i = 0; i < conta; i++) {
+            finale[i] = canzoniTrovate[i];
         }
         return finale;
     }
-    public String toString(){
+    public String toString() {
         String risultato = "[";
         for (int i = 0; i < miaPlaylist.length; i++) {
             if (miaPlaylist[i] != null) {
-                risultato += miaPlaylist[i];
+                risultato += miaPlaylist[i].toString();
                 if (i < miaPlaylist.length - 1) {
                     risultato += ", ";
                 }
--- src/TestPlaylist.java
+++ src/TestPlaylist.java
@@ -1,9 +1,21 @@
 public class TestPlaylist {
+
     public static void main(String[] args) {
-        Playlist playlist=new Playlist();
-        Canzone canzone1=new Canzone();
-        Canzone canzone2=new Canzone();
-        Canzone canzone3=new Canzone();
-        System.out.print("Numero di canzoni nella playlist: "+playlist.numeroCanzoni());
+        Playlist playlist = new Playlist();
+        System.out.println("Numero di canzoni presenti nella playlist: " + playlist.numeroCanzoni());
+        playlist.aggiungiCanzone(new Canzone("Brano 1", "Artista 1", 200));
+        playlist.aggiungiCanzone("Brano 2", "Artista 2", 180);
+        playlist.aggiungiCanzone("Brano 3", "Artista 3", 210);
+        System.out.println("Numero di canzoni presenti nella playlist dopo l'aggiunta: " + playlist.numeroCanzoni());
+        playlist.cancellaCanzone(new Canzone("Brano 2", "Artista 2", 180));
+        System.out.println("Numero di canzoni presenti nella playlist dopo la cancellazione: " + playlist.numeroCanzoni());
+        Canzone[] canzoniArtista1 = playlist.ricercaCanzoni("Artista 1");
+        System.out.println("Canzoni di Artista 1:");
+        for (int i = 0; i < canzoniArtista1.length; i++) {
+            Canzone canzone = canzoniArtista1[i];
+            System.out.println(canzone.getNomeCanzone() + " - " + canzone.getNomeCantante());
+        }
+        String artistaCanzone = playlist.ricercaCantante("Brano 3");
+        System.out.println("Il cantante della canzone 'Brano 3' è: " + artistaCanzone);
     }
 }
